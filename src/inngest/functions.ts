e940f48ab49f49cb205d5585edd3a095784e26cd
Your Name <bhanupss137@gmail.com>
2026-02-20 21:29:09 +0530

    new

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { Sandbox } from "@e2b/code-interpreter";
-import { anthropic, createAgent, createTool, createNetwork, type Tool, type Message, createState } from "@inngest/agent-kit";
+import { openai, createAgent, createTool, createNetwork, type Tool, type Message, createState } from "@inngest/agent-kit";
+
 import { prisma } from "@/lib/db";
 import { FRAGMENT_TITLE_PROMPT, PROMPT, RESPONSE_PROMPT } from "@/prompt";
 
@@ -61,11 +62,10 @@ export const codeAgentFunction = inngest.createFunction(
       name: "code-agent",
       description: "An expert coding agent",
       system: PROMPT,
-      model: anthropic({
-        model: "claude-3-5-sonnet",
+      model: openai({
+        model: "gpt-4o",
         defaultParameters: {
           temperature: 0.1,
-          max_tokens: 4096,
         },
       }),
       tools: [
@@ -195,11 +195,10 @@ export const codeAgentFunction = inngest.createFunction(
       name: "fragment-title-generator",
       description: "A fragment title generator",
       system: FRAGMENT_TITLE_PROMPT,
-      model: anthropic({
-        model: "claude-3-5-sonnet",
+      model: openai({
+        model: "gpt-4o-mini",
         defaultParameters: {
           temperature: 0.3,
-          max_tokens: 512,
         },
       }),
     })
@@ -208,11 +207,10 @@ export const codeAgentFunction = inngest.createFunction(
       name: "response-generator",
       description: "A response generator",
       system: RESPONSE_PROMPT,
-      model: anthropic({
-        model: "claude-3-5-sonnet",
+      model: openai({
+        model: "gpt-4o",
         defaultParameters: {
           temperature: 0.3,
-          max_tokens: 1024,
         },
       }),
     });
